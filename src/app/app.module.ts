53800efe1788ce8591bb00ff1fcac16880ee2aff
Abdo-xeno <naananiabdelkodouss@gmail.com>
2022-04-22 16:35:33 +0000

    added graph

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,8 @@ import { SystemFilesTreeComponent } from './system-files-tree/system-files-tree.
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxGraphModule
   ],
   providers: [],
   bootstrap: [AppComponent]
